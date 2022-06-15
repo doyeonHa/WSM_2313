b3179a3eef0646d4c687eddd93e51aaa8f4dfce7
doyeon <w2113@e-mirim.hs.kr>
2022-06-15 12:00:50 +0900

    js change toggle icon

--- 20220608_school cafeteria/js/main.js
+++ 20220608_school cafeteria/js/main.js
@@ -3,11 +3,15 @@ const toggleMenu = (toggleId, navListId) => {
     //html -> js
     const toggle = document.getElementById(toggleId);
     const navList = document.getElementById(navListId);
+    const toggleIcon = toggle.getElementsByTagName("i")[0];
 
     if (toggle && navList) {
         //toggle click
         toggle.addEventListener('click', () => {
             navList.classList.toggle('show-menu');
+            //change toggle icon: bx-menu <-> bx-x
+            toggleIcon.classList.toggle("bx-menu");
+            toggleIcon.classList.toggle("bx-x");
         });
         //show/hide menu: .show-menu
     }
