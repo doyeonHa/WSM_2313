490b6c6f0df6bd4911fdb8cde8281f24d047da09
doyeon <w2113@e-mirim.hs.kr>
2022-06-15 11:52:07 +0900

    js toggleMenu() 4

--- 20220608_school cafeteria/js/main.js
+++ 20220608_school cafeteria/js/main.js
@@ -4,15 +4,12 @@ const toggleMenu = (toggleId, navListId) => {
     const toggle = document.getElementById(toggleId);
     const navList = document.getElementById(navListId);
 
-    const clickHandler = () => {
-        //show/hide menu: .show-menu
-        navList.classList.toggle('show-menu');
-    }
-
     if (toggle && navList) {
         //toggle click
-        toggle.addEventListener('click', clickHandler);
-
+        toggle.addEventListener('click', () => {
+            navList.classList.toggle('show-menu');
+        });
+        //show/hide menu: .show-menu
     }
 }
 toggleMenu("nav-toggle", "nav-list");
